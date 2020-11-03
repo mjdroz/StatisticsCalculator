12d7e4524105e721b42a4dc7d0c0f1dadff21661
Michael Drozdowski <mjd76@njit.edu>
2020-11-03 13:03:58 -0500

    Fix: Correct file paths to fix the error when running the unit tests

--- UnitTests/test_calculator.py
+++ UnitTests/test_calculator.py
@@ -9,42 +9,42 @@ class MyTestCase(unittest.TestCase):
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, calculator)
-'''
+
     def test_add_method_calculator(self):
-        test_data = CSVReader('./TestData/Addition.csv').data
+        test_data = CSVReader('/UnitTests/TestData/Addition.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), float(row['Result']))
             self.assertEqual(self.calculator.result, float(row['Result']))
 
     def test_subtract_method_calculator(self):
-        test_data = CSVReader('./TestData/Subtraction.csv').data
+        test_data = CSVReader('/UnitTests/TestData/Subtraction.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.subtract(row['Value 2'], row['Value 1']), float(row['Result']))
             self.assertEqual(self.calculator.result,float(row['Result']))
 
     def test_multiplication_method_calculator(self):
-        test_data = CSVReader('./TestData/Multiplication.csv').data
+        test_data = CSVReader('/UnitTests/TestData/Multiplication.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), float(row['Result']))
             self.assertEqual(self.calculator.result,float(row['Result']))
 
     def test_division_method_calculator(self):
-        test_data = CSVReader('./TestData/Division.csv').data
+        test_data = CSVReader('/UnitTests/TestData/Division.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.divide(row['Value 2'], row['Value 1']), float(row['Result']))
             self.assertEqual(self.calculator.result, float(row['Result']))
 
     def test_square_method_calculator(self):
-        test_data = CSVReader('./TestData/Square.csv').data
+        test_data = CSVReader('/UnitTests/TestData/Square.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.square(row['Value 1']), float(row['Result']))
             self.assertEqual(self.calculator.result, float(row['Result']))
 
     def test_squareRoot_method_calculator(self):
-        test_data = CSVReader('./TestData/Subtraction.csv').data
+        test_data = CSVReader('/UnitTests/TestData/SquareRoot.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.squareRoot(row['Value 1']), float(row['Result']))
-            self.assertEqual(self.calculator.result, float(row['Result']))'''
+            self.assertEqual(self.calculator.result, float(row['Result']))
 
 if __name__ == '__main__':
     unittest.main()
