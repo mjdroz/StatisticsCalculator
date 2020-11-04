379f34b17192c66174c0bd0c8d3cf1be9b86d8a9
sordonez1996 <so235@njit.edu>
2020-11-03 19:09:26 -0500

    Task: Started the test function for mean

--- UnitTests/test_statistics.py
+++ UnitTests/test_statistics.py
@@ -1,10 +1,11 @@
 import unittest
-from numpy.random import random
+from numpy.random import randint
 from StatisticsCalc.statistics_calculator import statsCalc
 
 class MyTestCase(unittest.TestCase):
     def setUp( self ) -> None:
         self.statsCalc = statsCalc()
+        self.testData = numpy.randint()
 
     def test_instantiate_stats_calculator( self ):
         self.assertIsInstance(self.statsCalc, statsCalc)
