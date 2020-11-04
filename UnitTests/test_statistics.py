d3f3f4ec4d2b483aa603f6506a7f44cd13e3fcca
sordonez1996 <so235@njit.edu>
2020-11-03 19:04:22 -0500

    Feature: Ran and test instantiate stats calculator

--- UnitTests/test_statistics.py
+++ UnitTests/test_statistics.py
@@ -1,10 +1,13 @@
 import unittest
-
+from numpy.random import random
+from StatisticsCalc.statistics_calculator import statsCalc
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+    def setUp( self ) -> None:
+        self.statsCalc = statsCalc()
 
+    def test_instantiate_stats_calculator( self ):
+        self.assertIsInstance(self.statsCalc, statsCalc)
 
 if __name__ == '__main__':
     unittest.main()
