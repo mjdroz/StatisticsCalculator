37f6dacd7e1ada87159dfd9cc99a1a23211ca436
Michael Drozdowski <mjd76@njit.edu>
2020-11-04 20:24:32 -0500

    Fix: Assign self.testData to statistics data

--- UnitTests/test_statistics.py
+++ UnitTests/test_statistics.py
@@ -1,11 +1,12 @@
 import unittest
 from numpy.random import randint
 from StatisticsCalc.statistics_calculator import statsCalc
+from CSVReader.csv_reader import CSVReader
 
 class MyTestCase(unittest.TestCase):
     def setUp( self ) -> None:
         self.statsCalc = statsCalc()
-        self.testData = numpy.randint()
+        self.testData = CSVReader('/UnitTests/TestData/StatisticsTestData.csv')
 
     def test_instantiate_stats_calculator( self ):
         self.assertIsInstance(self.statsCalc, statsCalc)
