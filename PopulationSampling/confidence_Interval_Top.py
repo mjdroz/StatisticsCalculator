1232f57e407e315d12df8f5e79813bd7b8692254
sordonez1996 <so235@njit.edu>
2020-11-07 16:41:02 -0500

    Feature: Implemented confidence interval with a unit test

--- PopulationSampling/confidence_Interval_Top.py
+++ PopulationSampling/confidence_Interval_Top.py
@@ -4,10 +4,13 @@ from Calculator.multiplication import multiplication
 from Calculator.square_root import squareRoot
 from StatisticsCalc.mean import mean
 from StatisticsCalc.standard_deviation import standard_deviation
+from scipy import stats
 
 def confidenceIntervalTop (data, confidence_level):
     values = len(data)
     std = standard_deviation(data)
     avg = mean(data)
-    rounded_interval = round(addition(avg, multiplication(confidence_level, division(std, squareRoot(values)))), 5)
-    return rounded_interval
+    rounded_interval = stats.norm.interval(confidence_level, avg, division(std, squareRoot(values)))
+    intervalBottom = round(rounded_interval[1], 5)
+    # rounded_interval = round(addition(avg,multiplication(confidence_level,division(std,squareRoot(values)))), 5)
+    return intervalBottom
--- UnitTests/test_statistics.py
+++ UnitTests/test_statistics.py
@@ -69,8 +69,25 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(in_list, True)
 
     def test_confidence_interval_method (self):
-        pprint(self.statsCalc.confidenceIntervalBottom(self.testData, self.confidenceLevel))
-        self.assertEqual(True,True)
+        pprint("confidence interval bottom: " + str(self.statsCalc.confidenceIntervalBottom(self.testData, self.confidenceLevel)))
+        pprint("confidence interval top: " + str(self.statsCalc.confidenceIntervalTop(self.testData, self.confidenceLevel)))
+        test_values = stats.norm.interval(self.confidenceLevel, mean(self.testData), self.statsCalc.divide(std(self.testData), self.statsCalc.squareRoot(len(self.testData))))
+
+        confidence_bottom = self.statsCalc.confidenceIntervalBottom(self.testData, self.confidenceLevel)
+        confidence_top = self.statsCalc.confidenceIntervalTop(self.testData, self.confidenceLevel)
+        tester = False
+        for values in range(len(test_values)):
+            test = test_values[values]
+            rounded = round(test, 5)
+
+            if rounded == confidence_bottom:
+                tester = True
+            elif rounded == confidence_top:
+                tester = True
+            else:
+                tester = False
+
+        self.assertEqual(tester,True)
         '''confidence_bottom = self.statsCalc.confidenceIntervalBottom(self.testData, self.confidenceLevel)
         confidence_top = self.statsCalc.confidenceIntervalTop(self.testData, self.confidenceLevel)
         test_values = stats.norm.interval(self.confidenceLevel, mean(self.testData), std(self.testData))
